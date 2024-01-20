2908a57b327a7dff4025514be3dc6adc232ff2ea
Fachrifat <fachrifat05@gmail.com>
2024-01-20 20:29:19 +0700

    ganti dialec

--- Config/Database.js
+++ Config/Database.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME , process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'mysql2',
 })
 
 module.exports = db
