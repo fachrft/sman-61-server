dedd9a6d07263304e6486300acd1f2e51189e4b4
Fachrifat <fachrifat05@gmail.com>
2024-01-20 19:17:08 +0700

    hsoting database

--- Config/Database.js
+++ Config/Database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
+const dotenv = require("dotenv");
+dotenv.config()
 
-const db = new Sequelize('sman61', 'root', '' , {
-    host: 'localhost',
-    dialect : 'mysql'
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME , process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
 })
 
 module.exports = db
