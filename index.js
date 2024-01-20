723b44ef22243045c5a3cc89af16fb332a4a016a
Fachrifat <fachrifat05@gmail.com>
2024-01-20 20:59:14 +0700

    namabhin cors origin yang bener

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "https://nasa-pers.vercel.app/",
+        origin: "https://nasa-pers.vercel.app",
     })
 );
 
