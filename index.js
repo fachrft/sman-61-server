9cf6216029468a72320911d9505540da3ca0f95c
Fachrifat <fachrifat05@gmail.com>
2024-01-26 13:32:01 +0700

    gatau

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const db = require('./Config/Database.js')
-const path = require('path')
-const AdminRouter = require('./Router/AdminRoute.js')
-const ArtikelRouter = require('./Router/ArtikelRoute.js')
-const GalleryRouter = require('./Router/GalleryRoute.js')
-const MajalahRouter = require('./Router/MajalahRoute.js')
-const PojokKarya = require('./Router/PojokKaryaRoute.js');
+const db = require("./Config/Database.js");
+const path = require("path");
+const AdminRouter = require("./Router/AdminRoute.js");
+const ArtikelRouter = require("./Router/ArtikelRoute.js");
+const GalleryRouter = require("./Router/GalleryRoute.js");
+const MajalahRouter = require("./Router/MajalahRoute.js");
+const PojokKarya = require("./Router/PojokKaryaRoute.js");
 dotenv.config();
 const app = express();
 
@@ -20,21 +20,20 @@ const app = express();
 app.use(
     cors({
         credentials: true,
-        origin: "https://nasa-pers.vercel.app",
+        origin: "*",
     })
 );
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(AdminRouter)
-app.use(ArtikelRouter)
-app.use(GalleryRouter)
-app.use(MajalahRouter)
-app.use(PojokKarya)
+app.use(AdminRouter);
+app.use(ArtikelRouter);
+app.use(GalleryRouter);
+app.use(MajalahRouter);
+app.use(PojokKarya);
 
 app.listen(process.env.PORT, () => {
     console.log("listening on port " + process.env.PORT);
